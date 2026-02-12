5cf6baaccf06ea2b14e46a291dafa21c6911bb54
aaditya-arb <aaditya23dec@gmail.com>
2026-02-12 16:07:03 +0530

    UC1

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,11 @@
+/*
+use case 1 palindrome checker app
+Author: Aaditya Raj Bhatia
+*/
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome Checker App");
+        System.out.println("Version : 1.0");
+        System.out.println("System initiated successfully");
+    }
+}
