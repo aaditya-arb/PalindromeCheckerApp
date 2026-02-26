e3cabe6cc675f9f4b96c7d52e97c44ab3b73f824
aaditya-arb <aaditya23dec@gmail.com>
2026-02-26 14:57:50 +0530

    uc2 we are checking palindrome app

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,11 +1,39 @@
-/*
-use case 1 palindrome checker app
-Author: Aaditya Raj Bhatia
-*/
+/**
+ * =====================================================
+ * MAIN CLASS - UseCase2PalindromeCheckerApp
+ * =====================================================
+ *
+ * Use Case 2: Hardcoded Palindrome Validation
+ *
+ * Description:
+ * This class demonstrates basic palindrome validation
+ * using a hardcoded string value.
+ */
+
 public class PalindromeCheckerApp {
+
+    /**
+     * Application entry point for UC2.
+     * @param args Command-line arguments
+     */
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App");
-        System.out.println("Version : 1.0");
-        System.out.println("System initiated successfully");
+
+        String word = "madam";
+
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < word.length() / 2; i++) {
+
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
     }
 }
